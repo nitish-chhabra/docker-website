c38a9718f0f00ec4f9e464a5c42a02264195bd84
Nitish <nitish@goals101.in>
2021-03-12 20:04:24 +0530

    updating setup script for correct crontab addition

--- setup.sh
+++ setup.sh
@@ -2,9 +2,9 @@
 
 mkdir wordpress && cd wordpress
 mkdir nginx-conf
-cp ~/docker-website/nginx.conf nginx-conf/nginx.conf
+sudo cp /home/ec2-user/docker-website/nginx.conf nginx-conf/nginx.conf
+sudo curl -sSLo nginx-conf/options-ssl-nginx.conf https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf
+cd ..
 docker-compose up -d
-crontab -l > mycron
-echo "0 12 * * * /home/sammy/wordpress/ssl_renew.sh >> /var/log/cron.log 2>&1" >> mycron
-crontab mycron
-rm mycron
+sudo service crond start
+(crontab -l 2>/dev/null; echo "0 12 * * * /home/ec2-user/docker-website/ssl_renew.sh >> /var/log/cron.log 2>&1") | crontab -
